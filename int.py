ace37b6730dc1c23285ae9dd42f3a07c46707152
Isabella-TET <2425401837@live.gctu.edu.gh>
2026-05-31 11:31:13 +0000

    Update and rename initials.py to int.py

---
+++ int.py
@@ -0,0 +1,30 @@
+
+def main():
+# File: int.py
+# Description: Print out my initials
+# Assignment Number: Assignment 2
+#
+# Name: Isabella Nakwor Tetteh
+# STUDENT ID: 2425401837
+# Email: narkworxxx@gmail.com
+# Grader: Augustus Buckman
+#
+# On my honor, Isabella Nakwor Tetteh, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+print ()
+print ("...INT")
+print ()
+print ("...IIIIIIIIII.........NNNNN.........NNN........TTTTTTTTTT...")
+print ("...IIIIIIIIII.........NNNN.N........NNN........TTTTTTTTTT...")
+print ("......III.............NNN..N........NNN............TT.......")
+print ("......III.............NNN...N.......NNN............TT.......")
+print ("......III.............NNN....N......NNN............TT.......")
+print ("......III.............NNN.....N.....NNN............TT.......")
+print ("......III.............NNN......N....NNN............TT.......")
+print ("......III.............NNN.......N...NNN............TT.......")
+print ("...IIIIIIIIII...**....NNN........N.NNNN.....**.....TT.....**")
+print ("...IIIIIIIIII...**....NNN..........NNNNN.....**....TT.....**")
+
+
+main()
