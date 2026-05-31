653896663e3d92095158482b8a8a03793cd16098
Isabella-TET <2425401837@live.gctu.edu.gh>
2026-05-31 21:07:15 +0000

    Update intials.py

--- intials.py
+++ intials.py
@@ -1,32 +1,32 @@
-
-# File name: int.py
+# File name: intials.py
 # Description: Print out my initials
-# Assignment Number: Assignment 2
+# Assignment Number: Assignment 1
 #
 # Name: Isabella Nakwor Tetteh
 # STUDENT ID: 2425401837
-# Email: narkworxxx@gmail.com
+# Email: 2425401837@live.gctu.edu.gh
 # Grader: Augustus Buckman
-#
+
 # On my honor, Isabella Nakwor Tetteh, this programming assignment is my own work
 # and I have not provided this code to any other student.
 
+
 def main():
 # This assignment tasked me to print out my initials using the letters of my initials. 
-
 print ()
-print ("...INT")
-print ()
-print ("...IIIIIIIIII.........NNNNN.........NNN........TTTTTTTTTT...")
-print ("...IIIIIIIIII.........NNNN.N........NNN........TTTTTTTTTT...")
-print ("......III.............NNN..N........NNN............TT.......")
-print ("......III.............NNN...N.......NNN............TT.......")
-print ("......III.............NNN....N......NNN............TT.......")
-print ("......III.............NNN.....N.....NNN............TT.......")
-print ("......III.............NNN......N....NNN............TT.......")
-print ("......III.............NNN.......N...NNN............TT.......")
-print ("...IIIIIIIIII...**....NNN........N.NNNN......**....TT.....**")
-print ("...IIIIIIIIII...**....NNN..........NNNNN.....**....TT.....**")
+print ("...INT") 
+print() 
+print ("...IIIIIIIIIIII........NN........NN........TTTTTTTTTTTT.....")
+print ("...IIIIIIIIIIII........NNN.......NN........TTTTTTTTTTTT.....")
+print (".......III.............NNNN......NN............TTT..........")
+print (".......III.............NN.NN.....NN............TTT..........")
+print (".......III.............NN..NN....NN............TTT..........")
+print (".......III.............NN...NN...NN............TTT..........")
+print (".......III.............NN....NN..NN............TTT..........")
+print (".......III.............NN.....NN.NN............TTT..........")
+print ("...IIIIIIIIIIII...**...NN......NNNN...**.......TTT........**")
+print ("...IIIIIIIIIIII...**...NN.......NNN...**.......TTT........**")
+print()
 
 
 main()
